7c6c8a7eb0d36035c346cae26b8d8bae70eb6a83
khadada <lkhadada@gmail.com>
2022-03-31 10:47:38 +0200

    finish importing necessay modules and class, maybe I will import others If I have to

---
+++ appointment.py
@@ -0,0 +1,4 @@
+# Importing necessary modules and classes:
+from PyQt5.QtWidgets import (QApplication, QWidget,QFormLayout,QHBoxLayout,QLabel,QLineEdit,QPushButton,QSpinBox,QComboBox,QMessageBox,QCheckBox)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
